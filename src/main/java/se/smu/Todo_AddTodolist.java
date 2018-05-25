eaac8e2b6a2538e19fe2e0792336b4f307a1d31f
ulimy <ulim130@naver.com>
2018-05-25 15:39:48 +0900

    #1213 Todolist 기능 구현
    Add, Delete, Edit, Todolist 클래스 일부 구현

---
+++ src/main/java/se/smu/Todo_AddTodolist.java
@@ -0,0 +1,27 @@
+package se.smu;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Todo_AddTodolist implements Serializable{
+	
+	public static Todolist add(String tdlUrl,String s){
+		
+		//  확인을 위해 tdl이름만 넘겨받도록 구현,추후 GUI에서 넘겨받기
+		Todolist n = new Todolist("a",s,"c",null,true,true);
+		
+		// 파일에 저장
+		try {	
+			ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(tdlUrl,true));
+			writer.writeObject(n);
+			writer.close();	
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return n;
+
+ 	}
+}
