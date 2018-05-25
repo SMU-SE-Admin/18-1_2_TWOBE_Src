9f4b7505642c83c7587751a1ac502e32cdc8a2bb
ulimy <ulim130@naver.com>
2018-05-25 21:23:23 +0900

    #1211 수강과목 기능 구현
    수강과목 수정 기능

--- src/main/java/se/smu/Student.java
+++ src/main/java/se/smu/Student.java
@@ -6,8 +6,10 @@ import java.util.Arrays;
 
 public class Student{
 	int StudentNum;
-	int arrcount=0;
-	Todolist arr[] = new Todolist[100];
+	int t_arrcount=0;
+	int s_arrcount=0;
+	Todolist t_arr[] = new Todolist[100];
+	Subject s_arr[] = new Subject[100];
 	String tdlUrl;
 	String subUrl;
 	
@@ -17,21 +19,25 @@ public class Student{
 		this.subUrl = num+"_sub.txt";
 	}
 	
+	public void edit_sub(String subjectName,String professer,String subjectDay,int subjectTime,int runYear, int semester,String oldsub){
+		EditSubject.edit(subjectName,professer,subjectDay,subjectTime,runYear,semester,this.s_arr,this.s_arrcount,this.subUrl,oldsub);
+	}
+	
 	public void add_tdl(String s){
 		Todolist todo = Todo_AddTodolist.add(this.tdlUrl,s);
-		this.arr[this.arrcount] = todo;
-		this.arrcount++;
+		this.t_arr[this.t_arrcount] = todo;
+		this.t_arrcount++;
 		return;
 	}
 	
 	public void del_tdl(String a){
-		this.arr = Todo_DeleteTodolist.Delete(this.tdlUrl,this.arr,this.arrcount,a);
-		this.arrcount--;
+		this.t_arr = Todo_DeleteTodolist.Delete(this.tdlUrl,this.t_arr,this.t_arrcount,a);
+		this.t_arrcount--;
 		return;
 	}
 	
 	public void edit_tdl(String subjectName,String tdlName,String tdlDeadline,String tdlFinishDate,Boolean tdlCompleted,Boolean tdlImportant,String oldtd){
-		this.arr = Todo_EditTodolist.Edit(subjectName,tdlName,tdlDeadline,tdlFinishDate,tdlCompleted,tdlImportant,oldtd,this.tdlUrl,this.arr,this.arrcount);
+		this.t_arr = Todo_EditTodolist.Edit(subjectName,tdlName,tdlDeadline,tdlFinishDate,tdlCompleted,tdlImportant,oldtd,this.tdlUrl,this.t_arr,this.t_arrcount);
 		return;
 	}
 	
@@ -40,35 +46,36 @@ public class Student{
 	}
 	
 	public int getcount(){
-		return this.arrcount;
+		return this.t_arrcount;
 	}
 	
 	public Todolist[] getarr(){
-		return this.arr;
+		return this.t_arr;
 	}
+	
 	public void sort(String s){
 		switch(s){
 			case "name" :
-				Arrays.sort(this.arr,Todo_SortTodolist.NameComparator);
-				System.out.println(Arrays.toString(this.arr));
+				Arrays.sort(this.t_arr,Todo_SortTodolist.NameComparator);
+				System.out.println(Arrays.toString(this.t_arr));
 				break;
 			case "deadline"  :
-				Arrays.sort(this.arr,Todo_SortTodolist.DeadlineComparator);
-				System.out.println(Arrays.toString(this.arr));
+				Arrays.sort(this.t_arr,Todo_SortTodolist.DeadlineComparator);
+				System.out.println(Arrays.toString(this.t_arr));
 				break;
 			case "finishDate" :
-				Todo_SortTodolist.FinishDate_sort(this.arr);
-				System.out.println(Arrays.toString(this.arr));
+				Todo_SortTodolist.FinishDate_sort(this.t_arr);
+				System.out.println(Arrays.toString(this.t_arr));
 				
 				break;
 			case "completed" :
-				Todo_SortTodolist.Completed_sort(this.arr);
-				System.out.println(Arrays.toString(this.arr));
+				Todo_SortTodolist.Completed_sort(this.t_arr);
+				System.out.println(Arrays.toString(this.t_arr));
 		
 		}	
 	}
 	public void bookMark(){
-		Todo_BookmarkTodolist.bookMark(this.arr);
-		System.out.println(Arrays.toString(this.arr));
+		Todo_BookmarkTodolist.bookMark(this.t_arr);
+		System.out.println(Arrays.toString(this.t_arr));
 	}
 }
