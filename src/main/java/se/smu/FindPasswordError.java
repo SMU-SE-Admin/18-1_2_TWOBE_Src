ce7dff357d967b028ba1dc6b7f571f664ad259c5
201411096 <korea5781@naver.com>
2018-05-26 16:07:52 +0900

    #1210 UI 구현
    일부 내용 수정 및 비밀번호 찾기 오류 창 생성

--- src/main/java/se/smu/FindPasswordError.java
+++ src/main/java/se/smu/FindPasswordError.java
@@ -1,13 +1,14 @@
 package se.smu;
+
 import javax.swing.*;
 import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class LoginError extends JFrame{
+public class FindPasswordError extends JFrame{
 	//private Font f = new Font("돋움", Font.BOLD, 20);
 	
-	public LoginError() {
+	public FindPasswordError() {
 		setTitle("실행 오류");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -15,18 +16,18 @@ public class LoginError extends JFrame{
 		c.setBackground(new Color(184,255,255));
 		c.setLayout(null);
 		
-		JLabel em = new JLabel("아이디와 비밀번호를 확인해주세요");
+		JLabel em = new JLabel("학번과 질문을 확인해주세요");
 		JButton ob = new JButton("확인");
 		
 		c.add(em);
 		c.add(ob);
 		
-		em.setLocation(70, 15);
+		em.setLocation(100, 15);
 		em.setSize(250, 50);
 		ob.setLocation(140, 85);
 		ob.setSize(70, 25);
 		
-		JLabel em2 = new JLabel("(请检查您的ID和密码。)");
+		JLabel em2 = new JLabel("(請檢查學生人數和問題)");
 		c.add(em2);
 		em2.setLocation(110, 35);
 		em2.setSize(250, 50);
@@ -40,19 +41,17 @@ public class LoginError extends JFrame{
 		setVisible(true);
 		
 		ob.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
+			public void actionPerformed(ActionEvent e) {				
 				if(true) {
 					dispose();		      
 				}
-
 			}
 		});
 		
 	}
 	
 	public static void main(String[] args) {
-		//LoginError loginerror = new LoginError();
+		//FindPasswordError findPasswordError = new FindPasswordError();
 	}
 
 }
