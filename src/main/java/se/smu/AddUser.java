5de702287fade4bae60c42be6a96f1d63af9773e
201411096 <korea5781@naver.com>
2018-05-27 19:10:54 +0900

    #1210 UI구현
    일부 내용 수정

--- src/main/java/se/smu/AddUser.java
+++ src/main/java/se/smu/AddUser.java
@@ -1,30 +1,64 @@
-package se.smu;
+package test_signup;
 
-
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+public class AddUser {
+	 
+    private String filePath;
+    private User user;
+    public static void main(String[] args){
+         
+    	AddUser main = new AddUser();
+        try {
+            main.init();
+            main.doSerializable();
+            User user = (User) main.undoSerializable();
+             
+           // System.out.println(user.getStudentName());
 
-public class AddUser implements Serializable{
-	
-	//수정 전 public static User add(String siUrl,String s){
-		public static User add(String siUrl, String a, String b, String c, String d) {
-		//  확인을 위해 tdl이름만 넘겨받도록 구현,추후 GUI에서 넘겨받기
-		User n = new User(a,b,c,d);
-		
-		// 파일에 저장
-		try {	
-			ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(siUrl,true));
-			writer.writeObject(n);
-			writer.close();	
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-		return n;
-
- 	}
+             
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+     
+    public void init() {
+ 
+ 
+         
+        user = new User();
+       // user.setName("kim dong");
+       // user.setAge(50);
+       // user.setPhoneNumber("010-9858-9985");
+        
+        filePath = "C:\\Users\\aaaaaa\\eclipse-workspace\\TWOBE\\"+user.getStudentNum()+".ser";
+    }
+     
+    // 마샬링(marshalling) 수행, 직렬화
+    public void doSerializable() throws IOException {
+ 
+        FileOutputStream fos = new FileOutputStream(filePath);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(user);
+        oos.close();
+    }
+     
+    // 언마샬링(unmarshalling) 수행, 역직렬화
+    public Object undoSerializable() throws IOException, ClassNotFoundException {
+ 
+        FileInputStream fis = new FileInputStream(filePath);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Object user = (User) ois.readObject();
+        ois.close();
+         
+        return user;
+    }
 }
+
+
--- src/main/java/se/smu/User.java
+++ src/main/java/se/smu/User.java
@@ -1,33 +1,48 @@
-package se.smu;
+package test_signup;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-public class User implements Serializable{
+public class User {
+    private static final long serialVersionUID = 2L;
+     private String studentNum;
 	 private String studentName;
 	 private String major;
 	 private String password;
 	 private String ques;
-
-	   
-	 public User(String studentName,String major, String password,String ques){
-	      this.studentName = studentName;
-	      this.major = major;
-	      this.password = password;
-	      this.ques = ques;
-	 }
+     
 	 
-	   
-	 public String getStudentName(){
-		 return this.studentName;
-	 }
-	 public String getMajor(){
-	    return this.major;
-	 }
-	 public String getPassword(){
-	    return this.password;
-	 }
-	 public String getQues(){
-		 return this.ques;
-	 }
-
-
+	public String getStudentNum() {
+	    return studentName;
+	}
+	public void setStudentNum(String studentNum) {
+	    this.studentNum = studentNum;
+	}
+    public String getStudentName() {
+        return studentName;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getQues() {
+        return ques;
+    }
+    public void setQues(String password) {
+        this.ques = ques;
+    }
 }
